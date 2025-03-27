6762bc94fe14930f259ca007eaacf5b4a4be6893
diki <muliaandiki@gmaiil.com>
2025-03-27 13:02:28 +0700

    slicing join the community

--- resources/views/Home.blade.php
+++ resources/views/Home.blade.php
@@ -48,18 +48,18 @@
         <div class="flex justify-center py-[2rem]">
           <h1 class="font-bold text-white :">Tools :</h1>
         </div>
-        <div class="grid grid-cols-4 grid-rows-1 gap-[3rem] py-[1rem] px-8">
-          <div class="border-2">
+        <div class="grid grid-cols-4 grid-rows-1 gap-[3rem] py-[1rem] px-8 h-[16rem]">
+          <div class="border-2 rounded-md">
             <img class="" src="" alt="foto-1">
 
           </div>
-          <div class="border-2 h-[16rem] rounded-md">
+          <div class="border-2 rounded-md">
             <img class="object-cover" src="" alt="foto-2">
           </div>
-          <div class="border-2">
+          <div class="border-2 rounded-md">
             <img class="object-cover" src="" alt="foto-3">
           </div>
-          <div class="border-2">
+          <div class="border-2 rounded-md">
             <img class="object-cover" src="" alt="foto-4">
           </div>
         </div>
@@ -123,9 +123,9 @@
                       <p class=""></p>
                     </div>
                   </a>
-
                 </div>
-            </div>
+
+              </div>
 
             {{-- Video --}}
             <div class="flex justify-center pt-[2rem]" id="container-video">
@@ -134,13 +134,13 @@
               </div>
             </div>
 
-            <div id="tabel" class="flex justify-center">
+            <div id="tabel" class="flex justify-center border-b-2 pb-[2rem]">
               <div class="grid grid-cols-1 grid-rows-7 gap-4 w-[80%]">
 
                 <a href="">
                     <div class="flex justify-between border-b-2">
                       <h1 class="">1. Lorem</h1>
-                      <p class=""> Last</p>
+                      <p class="">Last</p>
                     </div>
                 </a>
 
@@ -180,17 +180,124 @@
                 </a>
 
                 <a href="">
-                    <div class="flex justify-between border-b-2">
+                    <div class="flex justify-between border-b-2 ">
                       <h1 class="">7.</h1>
                       <p class=""></p>
                     </div>
                 </a>
-
+              </div>
+            </div>
+            
+            {{-- Join the Community --}}
+            <div class="flex justify-center">
+              <div class="" id="Community">
+                  <h1 class="font-bold text-[2rem]">Join the Community</h1>
               </div>
             </div>
 
+            <div class="flex justify-center items-center">
+              <p class="font-light">roadmap.sh is the 7th most starred project on GitHub and is visited by hundreds of thousands of developers every month.</p>
+            </div>
+
+            <div class="flex justify-center items-center">
+              <div class="grid grid-cols-3 grid-rows-1 gap-4">
+                <div class="border-r-1">
+                  <div class="flex justify-center items-center">
+                    <div>
+                        <div title="join the comunity">
+                          <p class="text-blue-500">Rank 7th out of 28M!</p>
+                        </div>
+      
+                        <div class="flex justify-center items-center">
+                          <h1 class="text-[2rem] font-bold">313K</h1>
+                        </div>
+      
+                        <div class="flex justify-center items-center">
+                          <p class="font-light">GitHub Star</p>
+                        </div>
+      
+                        <div class="flex justify-center items-center">
+                          <div class="border-2 w-[13vw] h-[7vh] rounded-lg flex flex-col justify-center items-center">
+                            <h1 class="font-bold">Star us on GitHub</h1>
+                            <p class="font-light">Help you reach #1</p>
+                          </div>
+                      </div>
+                    </div>
+
+                  </div>
+                </div>
+
+                <div class="border-r-1">
+                  <div class="flex justify-center items-center">
+                    <div>
+                        <div title="join the comunity">
+                          <p class="text-blue-500">+90k every month</p>
+                        </div>
+                        
+                        <div class="flex justify-center items-center">
+                          <h1 class="text-[2rem] font-bold">+1.5M</h1>
+                        </div>
+      
+                        <div class="flex justify-center items-center">
+                          <p class="font-light">Register User</p>
+                        </div>
+
+                        <div class="flex justify-center items-center">
+                          <div class="border-2 w-[13vw] h-[7vh] rounded-lg flex flex-col justify-center items-center">
+                            <h1 class="font-bold">Register yourself</h1>
+                            <p class="font-light">Commit to your growth </p>
+                          </div>
+                      </div>
+
+                    </div>
+                    </div>
+                  
+                </div>
+
+                <div class="border-r-1">
+                  <div class="flex justify-center items-center">
+                    <div>
+                        <div title="join the comunity">
+                          <p class="text-blue-500">+2k every month</p>
+                        </div>
+          
+      
+                        <div class="flex justify-center items-center">
+                          <p class="text-[2rem] font-bold">35K</p>
+                        </div>
+      
+                        <div class="flex justify-center items-center">
+                          <p class="font-light">Discord Member</p>
+                        </div>
+
+                        <div class="flex justify-center items-center">
+                          <div class="border-2 w-[13vw] h-[7vh] rounded-lg flex flex-col justify-center items-center">
+                            <h1 class="font-bold">Join On Discord</h1>
+                            <p class="font-light">join the comunity</p>
+                          </div>
+
+                      </div>
+
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            {{-- Batas-bg-putih --}}
         </div>
-        {{-- Mulai Bg-Hitam --}}
+
+
+        {{-- Footer --}}
+        <footer>
+          <div class="bg-slate-900 h-[30vh] w-screen">
+            <div id="footer-container">
+              <div class="flex justify-center items-center">
+                <h1 class="text-white">Ini footer</h1>
+              </div>
+            </div>
+          </div>
+        </footer>
     </div>
 </body>
 </html>
