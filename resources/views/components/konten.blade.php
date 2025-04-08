3b04092c4d402fa4c74c939e69080ef1e0a3e193
diki <muliaandiki@gmaiil.com>
2025-04-08 15:19:32 +0700

    fixed

--- resources/views/components/konten.blade.php
+++ resources/views/components/konten.blade.php
@@ -1,11 +1,11 @@
 @vite('resources/css/app.css')
 @vite('resources/js/app.js')
 <div class="flex justify-center">
-    <div class="h-[1vh] w-[5vw] text-center py-[1rem]">
-        <div class="border-2 rounded-md bg-yellow-300 ">
+    <div class="h-[1vh] w-[6vw] text-center py-[1rem]">
+        <div class="border-2 rounded-md bg-yellow-300 hover:bg-slate-600 duration-[0.3s] hover:text-white p-2">
             <button class="">HTML</button>
         </div>
-        <div class="border-2 rounded-md bg-yellow-300 ">
+        <div class="border-2 rounded-md bg-yellow-300 hover:bg-slate-600 duration-[0.3s] hover:text-white p-2">
             <button class="">Css</button>
         </div>
     </div>
